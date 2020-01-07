65312e47a8d543047d824c834fce328f5aef5bfa
Ben Clark <BClark@ih-corp.com>
2020-01-07 11:27:54 -0500

    Creating data model and db context

---
+++ WishList/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.EntityFramewirkCore;
+using WishList.Models;
+
+namespace WishList.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Item> Items { get; set; }
+
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
